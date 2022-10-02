d9e1606796cf229211ea039b24d47e184b0bebc5
itsgosho <itsgosho2@gmail.com>
2022-10-02 20:48:29 +0300

    Fixed tile size

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,21 @@
 #define OLED_WIDTH 128
 #define OLED_HEIGHT 32
 
-#define TILE_SIZE 7
+#define TILE_SIZE 14
 #define TILE_MARGIN 10
 
-uint8_t leftTileHead;
-uint8_t leftTileTail;
-uint8_t rightTileHead;
-uint8_t rightTileTail;
+struct Point {
+    uint8_t x;
+    uint8_t y;
+};
+
+struct Tile {
+    Point head;
+    Point tail;
+};
+
+Tile leftTile;
+Tile rightTile;
 
 //starting logic
 void drawBorders(Adafruit_SSD1306& display) {
@@ -39,12 +47,16 @@ void drawInitialLeftTile(Adafruit_SSD1306& display, uint8_t tileSize, uint8_t ti
 
     display.drawPixel(tileStartingPosition, tileCenter, WHITE);
 
-    for (uint8_t i = 1; i < tileSize; ++i) {
+    for (uint8_t i = 1; i < tileSize / 2; ++i) {
         display.drawPixel(tileStartingPosition, tileCenter - i, WHITE);
         display.drawPixel(tileStartingPosition, tileCenter + i, WHITE);
     }
 
     display.display();
+
+    Point tileHead{tileStartingPosition, (uint8_t) (tileCenter - ((tileSize / 2) - 1))};
+    Point tileTail{tileStartingPosition, (uint8_t) (tileCenter + ((tileSize / 2) - 1))};
+    leftTile = Tile{tileHead, tileTail};
 }
 
 void drawInitialRightTile(Adafruit_SSD1306& display, uint8_t tileSize, uint8_t tileMargin) {
@@ -55,7 +67,7 @@ void drawInitialRightTile(Adafruit_SSD1306& display, uint8_t tileSize, uint8_t t
 
     display.drawPixel(tileStartingPosition, tileCenter, WHITE);
 
-    for (uint8_t i = 1; i < tileSize; ++i) {
+    for (uint8_t i = 1; i < tileSize / 2; ++i) {
         display.drawPixel(tileStartingPosition, tileCenter - i, WHITE);
         display.drawPixel(tileStartingPosition, tileCenter + i, WHITE);
     }
@@ -69,6 +81,10 @@ void drawInitialTiles(Adafruit_SSD1306& display, uint8_t tileSize, uint8_t tileM
     drawInitialRightTile(display, tileSize, tileMargin);
 }
 
+/*TODO: Mark the head and tail for the left/right tails. (Try to calculate them after the cycle or before not update every cycle)
+ * Make a mocked button logic.
+ * On each button press implement the logic from the excali
+ * */
 void setup() {
 
     Wire.setPins(OLED_SDA_PIN, OLED_SCL_PIN);
