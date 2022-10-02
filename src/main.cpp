0230e294fb34a3f7fe4e721dacbd2b535b7eab8c
itsgosho <itsgosho2@gmail.com>
2022-10-02 04:36:43 +0300

    borders logic drawing moved

--- src/main.cpp
+++ src/main.cpp
@@ -8,32 +8,9 @@
 #define OLED_WIDTH 128
 #define OLED_HEIGHT 32
 
-void setup() {
+#define TILE_SIZE 10
 
-    Wire.setPins(OLED_SDA_PIN, OLED_SCL_PIN);
-
-    Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT, &Wire);
-    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-
-    delay(2000);
-    /* display.clearDisplay();
-
-     display.setTextSize(1);
-     display.setTextColor(WHITE);
-     display.setCursor(0, 10);
-     // Display static text
-     display.println("Hello, world!");
-     display.display();*/
-
-    /* display.clearDisplay();
-     display.drawPixel(0, 0, WHITE);
-     display.drawPixel(0, 18, WHITE);
-     delay(1000);
-     display.drawPixel(0, 18, BLACK);
-     display.display();*/
-
-    display.clearDisplay();
-
+void drawBorders(Adafruit_SSD1306& display) {
     for (int16_t column = 0; column < OLED_WIDTH; ++column) {
         display.drawPixel(column, 0, WHITE);
         display.drawPixel(column, OLED_HEIGHT - 1, WHITE);
@@ -45,6 +22,22 @@ void setup() {
     }
 
     display.display();
+}
+
+
+void setup() {
+
+    Wire.setPins(OLED_SDA_PIN, OLED_SCL_PIN);
+
+    Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT, &Wire);
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
+    delay(2000);
+
+    display.clearDisplay();
+
+    drawBorders(display);
+
 
 }
 
