bba36028f2ef7969b696a2d5f93c3f81ccc5c19e
itsgosho <itsgosho2@gmail.com>
2022-10-11 15:12:26 +0300

    Tile moving logic set

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,13 @@ using namespace itsgosho;
 
 
 struct Tile {
-    TwoDRObject object;
+    TwoDRObject* object;
     Direction direction;
+
+    Tile(TwoDRObject* object, Direction direction) {
+        this->object = object;
+        this->direction = direction;
+    }
 };
 
 Adafruit_SSD1306* oledDisplay;
@@ -26,15 +31,15 @@ Adafruit_SSD1306* oledDisplay;
 ButtonEnhanced leftTileBtn;
 ButtonEnhanced rightTileBtn;
 
-TwoDRObject tileLeftObject;
-TwoDRObject tileRightObject;
-TwoDRObject topBorderObject;
-TwoDRObject bottomBorderObject;
-TwoDRObject leftBorderObject;
-TwoDRObject rightBorderObject;
+TwoDRObject* tileLeftObject;
+TwoDRObject* tileRightObject;
+TwoDRObject* topBorderObject;
+TwoDRObject* bottomBorderObject;
+TwoDRObject* leftBorderObject;
+TwoDRObject* rightBorderObject;
 
-Tile tileLeft;
-Tile tileRight;
+Tile* tileLeft;
+Tile* tileRight;
 
 void setup() {
     Serial.begin(9600);
@@ -52,24 +57,24 @@ void setup() {
 
     oledDisplay->clearDisplay();
 
-    tileLeftObject = TwoDRObject(3, 11, oledDisplay);
-    tileRightObject = TwoDRObject(3, 11, oledDisplay);
+    tileLeftObject = new TwoDRObject(3, 11, oledDisplay);
+    tileRightObject = new TwoDRObject(3, 11, oledDisplay);
 
-    tileLeftObject.draw({15, OLED_HEIGHT / 2 - 1}, InnerPosition::C);
-    tileRightObject.draw({OLED_WIDTH - 15 - 1, OLED_HEIGHT / 2 - 1}, InnerPosition::C);
+    tileLeftObject->draw({15, OLED_HEIGHT / 2 - 1}, InnerPosition::C);
+    tileRightObject->draw({OLED_WIDTH - 15 - 1, OLED_HEIGHT / 2 - 1}, InnerPosition::C);
 
-    tileLeft = {tileLeftObject, UP};
-    tileRight = {tileRightObject, UP};
+    tileLeft = new Tile(tileLeftObject, UP);
+    tileRight = new Tile(tileRightObject, UP);
 
-    topBorderObject = TwoDRObject(OLED_WIDTH - 1, 1, oledDisplay);
-    bottomBorderObject = TwoDRObject(OLED_WIDTH - 1, 1, oledDisplay);
-    leftBorderObject = TwoDRObject(1, OLED_HEIGHT - 2 - 1, oledDisplay);
-    rightBorderObject = TwoDRObject(1, OLED_HEIGHT - 2 - 1, oledDisplay);
+    topBorderObject = new TwoDRObject(OLED_WIDTH - 1, 1, oledDisplay);
+    bottomBorderObject = new TwoDRObject(OLED_WIDTH - 1, 1, oledDisplay);
+    leftBorderObject = new TwoDRObject(1, OLED_HEIGHT - 2 - 1, oledDisplay);
+    rightBorderObject = new TwoDRObject(1, OLED_HEIGHT - 2 - 1, oledDisplay);
 
-    topBorderObject.draw({0, 0}, InnerPosition::TL);
-    bottomBorderObject.draw({0, OLED_HEIGHT - 1}, InnerPosition::TL);
-    leftBorderObject.draw({0, 1}, InnerPosition::TL);
-    rightBorderObject.draw({OLED_WIDTH - 1, 1}, InnerPosition::TL);
+    topBorderObject->draw({0, 0}, InnerPosition::TL);
+    bottomBorderObject->draw({0, OLED_HEIGHT - 1}, InnerPosition::TL);
+    leftBorderObject->draw({0, 1}, InnerPosition::TL);
+    rightBorderObject->draw({OLED_WIDTH - 1, 1}, InnerPosition::TL);
 
     TwoDRObject pixelBall = TwoDRObject(5, 5, oledDisplay);
     pixelBall.draw({OLED_WIDTH / 2, OLED_HEIGHT / 2}, InnerPosition::C);
@@ -109,21 +114,23 @@ void setup() {
 
 }
 
-void moveTile(Tile& tile) {
-    if (tile.object.isUpMoveCollision(topBorderObject)) {
-        tile.direction = tile.direction == UP ? DOWN : UP;
+void moveTile(Tile* tile) {
+    if (tile->object->isUpMoveCollision(*topBorderObject) || tile->object->isDownMoveCollision(*bottomBorderObject)) {
+        tile->direction = tile->direction == UP ? DOWN : UP;
     }
 
-    tile.object.move(tile.direction);
+    tile->object->move(tile->direction);
 }
 
 void loop() {
 
     if (leftTileBtn.isShot() || leftTileBtn.isHold()) {
         moveTile(tileLeft);
+        oledDisplay->display();
     }
 
     if (rightTileBtn.isShot() || rightTileBtn.isHold()) {
         moveTile(tileRight);
+        oledDisplay->display();
     }
 }
